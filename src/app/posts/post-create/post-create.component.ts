1640db1ca9c656100406b3334fb5da4509676cd5
Rebecca Dillon <n0313191@lm.lmig.com>
2018-07-26 15:20:42 +0100

    section 2 lectures 22-24, form handling

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 import {Post} from '../post.model';
 
@@ -12,12 +13,17 @@ export class PostCreateComponent {
   enteredContent: '';
   enteredTitle: '';
 
-  // EventEmitter is a generic type. to change what it emits you enter <>. It means you can restrict what data you are emitting. 
+  // EventEmitter is a generic type. to change what it emits you enter <>. It means you can restrict what data you are emitting.
   @Output() // this turns postCreated into an event you can listen to elsewhere ie the parent component (app.component.html)
   postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
-    const post: Post = {title: this.enteredTitle, content: this.enteredContent};
+  onAddPost(form: NgForm ) {
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content};
     // emits the post as an arguement
     this.postCreated.emit(post);
   }
